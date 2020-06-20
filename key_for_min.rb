fc5e24a7951dbdf9f5d84d1ba007cc9da2c156aa
Deborah Santillan <s.debb@yahoo.com>
2020-06-20 18:16:41 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  values = name_hash.values
+  name_hash.keys[values.index(values.min).to_i]
 end
