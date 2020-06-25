c1813896ef10285c8a5a35d4394e655e7ea314c4
Cameron Hudson <camnn3@yahoo.com>
2020-06-25 17:48:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_value = hash.reduce{|key, value|key.last > value.last ? value : key}
+  smallest_value = hash.reduce { |key, value|key.last > value.last ? value : key}
     if name_hash == {}
       smallest_value
     else
