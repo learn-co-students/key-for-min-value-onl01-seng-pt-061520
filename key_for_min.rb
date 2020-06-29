7e33d444177fee2e09ae64ec97186bc54e874515
Dazha Brown <dajahbrown@hotmail.com>
2020-06-29 05:32:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,15 @@
 
 def key_for_min_value(name_hash)
   min_value = nil
-    min_key = nil
-    name_hash.each do |k, v|
-      if min_value == nil
-        min_value = v
-        min_key = k
-      elsif min_value > v
-        min_value = v
-        min_key = k
-      end
+  min_key = nil
+  name_hash.each do |k, v|
+    if min_value == nil
+      min_value = v
+      min_key = k
+    elsif min_value > v
+      min_value = v
+      min_key = k
     end
-    min_key
+  end
+  min_key
 end
